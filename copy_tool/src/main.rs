8933974d8d55476513ccff9a404970f1c2082c98
Flurin Feuerstein <flufeu@gmail.com>
2024-03-27 22:27:32 +0100

    woooorkiiing :-D

--- copy_tool/src/main.rs
+++ copy_tool/src/main.rs
@@ -2,26 +2,28 @@ mod serial_wrapper;
 
 use crate::serial_wrapper::SerialWrapper;
 use serial2::SerialPort;
-use std::path::PathBuf;
+use std::path::Path;
 
 fn main() -> Result<(), String> {
     let matching_device = get_matching_device().ok_or("Unable to find device".to_owned())?;
 
+    println!("Found Device {}", matching_device);
+
     matching_device
-        .write("PASSWORD")
+        .write("FUCK YOU")
         .map_err(|e| format!("{e}"))
 }
 
 fn get_matching_device() -> Option<SerialWrapper> {
     let available_ports = SerialPort::available_ports().ok()?;
     let matching_device = available_ports.iter().find(|a| is_correct_device(*a))?;
-    SerialWrapper::new(matching_device).ok()
+    SerialWrapper::new(matching_device.to_owned()).ok()
 }
 
-fn is_correct_device(device: &PathBuf) -> bool {
+fn is_correct_device(device: &Path) -> bool {
     println!("Checking Port {:?}", device);
 
-    let port = match SerialWrapper::new(device) {
+    let port = match SerialWrapper::new(device.to_owned()) {
         Ok(port) => port,
         _ => return false,
     };
@@ -34,6 +36,6 @@ fn is_correct_device(device: &PathBuf) -> bool {
         Ok(message) => message,
         Err(_) => return false,
     };
-    
+
     return message == "Fuck YOU";
 }
--- copy_tool/src/serial_wrapper.rs
+++ copy_tool/src/serial_wrapper.rs
@@ -1,15 +1,22 @@
-use std::{io, path::Path, thread::sleep, time};
+use std::{fmt::Display, io, path::PathBuf, thread::sleep, time};
 
 use serial2::SerialPort;
 
 pub struct SerialWrapper {
     port: SerialPort,
+    path: PathBuf,
+}
+
+impl Display for SerialWrapper {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.path.to_str().unwrap_or("Unknown"))
+    }
 }
 
 impl SerialWrapper {
-    pub fn new(path: impl AsRef<Path>) -> io::Result<Self> {
-        let port = SerialPort::open(path, 115200)?;
-        Ok(Self { port })
+    pub fn new(path: PathBuf) -> io::Result<Self> {
+        let port = SerialPort::open(path.clone(), 115200)?;
+        Ok(Self { port, path })
     }
 
     pub fn write(&self, value: &str) -> io::Result<()> {
@@ -20,7 +27,7 @@ impl SerialWrapper {
 
     pub fn read(&self) -> io::Result<String> {
         let mut buffer = [0; 64];
-        
+
         let sleep_duration = time::Duration::from_millis(5);
         sleep(sleep_duration);
 
