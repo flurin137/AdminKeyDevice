b524e41001ae5961dd7bce251187a5e13215d1b5
Flurin Feuerstein <flufeu@gmail.com>
2024-03-28 18:16:43 +0100

    moved adminkey to own mod

---
+++ copy_tool/src/device.rs
@@ -0,0 +1,51 @@
+use serial2::SerialPort;
+
+use crate::serial_wrapper::SerialWrapper;
+
+pub struct AdminKey {
+    serial_wrapper: SerialWrapper,
+}
+
+impl AdminKey {
+    pub fn connect() -> Result<Self, String> {
+        let serial_wrapper = get_serial_port().ok_or("err")?;
+
+        println!("Found Device {}", serial_wrapper);
+
+        Ok(Self { serial_wrapper })
+    }
+
+    pub fn write(&self, message: &str) -> Result<(), String> {
+        self.serial_wrapper
+            .write(message)
+            .map_err(|e| format!("{e}"))
+    }
+}
+
+fn get_serial_port() -> Option<SerialWrapper> {
+    let available_ports = SerialPort::available_ports().ok()?;
+
+    for port in available_ports {
+        println!("Checking Port {:?}", port);
+
+        let port = match SerialWrapper::new(port.to_owned()) {
+            Ok(port) => port,
+            _ => continue,
+        };
+
+        if let Err(_) = port.write("Whaaat") {
+            continue;
+        }
+
+        let message = match port.read() {
+            Ok(message) => message,
+            Err(_) => continue,
+        };
+
+        if message == "Fuck YOU" {
+            return Some(port);
+        }
+    }
+
+    None
+}
--- copy_tool/src/main.rs
+++ copy_tool/src/main.rs
@@ -1,42 +1,9 @@
+mod device;
 mod serial_wrapper;
 
-use crate::serial_wrapper::SerialWrapper;
-use serial2::SerialPort;
+use crate::device::AdminKey;
 
 fn main() -> Result<(), String> {
-    let matching_device = get_matching_device().ok_or("Unable to find device".to_owned())?;
-
-    println!("Found Device {}", matching_device);
-
-    matching_device
-        .write("FUCK YOU")
-        .map_err(|e| format!("{e}"))
-}
-
-fn get_matching_device() -> Option<SerialWrapper> {
-    let available_ports = SerialPort::available_ports().ok()?;
-
-    for port in available_ports {
-        println!("Checking Port {:?}", port);
-
-        let port = match SerialWrapper::new(port.to_owned()) {
-            Ok(port) => port,
-            _ => continue,
-        };
-
-        if let Err(_) = port.write("Whaaat") {
-            continue;
-        }
-
-        let message = match port.read() {
-            Ok(message) => message,
-            Err(_) => continue,
-        };
-
-        if message == "Fuck YOU" {
-            return Some(port);
-        }
-    }
-
-    None
+    let device = AdminKey::connect()?;
+    device.write("FUCK YOU")
 }
