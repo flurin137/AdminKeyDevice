f5efc0d27f5b46a8434c5cd7c65ec32cc14f9632
Flurin Feuerstein <flufeu@gmail.com>
2024-03-28 18:56:39 +0100

    added more validation

--- copy_tool/src/validation.rs
+++ copy_tool/src/validation.rs
@@ -1,13 +1,22 @@
-pub struct Validator;
+use regex::Regex;
+
+pub struct Validator {
+    regex: Regex,
+}
 
 impl Validator {
     pub fn new() -> Self {
-        Self
+        let regex = Regex::new(r"^[\w\d]+$").unwrap();
+        Self { regex }
     }
 
     pub fn validate(&self, value: String) -> Result<String, String> {
         if value.len() > 64 {
             return Err("String is too long".to_owned());
+        }
+
+        if !self.regex.is_match(&value) {
+            return Err("The string contains invalid characters".to_owned());
         }
 
         Ok(value)
