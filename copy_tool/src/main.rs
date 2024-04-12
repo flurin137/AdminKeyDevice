f6b3f09304ee971f33d71ed04d8385285fa0cc79
Feuerstein Flurin <flurin.feuerstein@gritec.ch>
2024-04-12 16:32:24 +0200

    readded clipperrs

--- copy_tool/src/main.rs
+++ copy_tool/src/main.rs
@@ -1,4 +1,4 @@
-//mod clipboard;
+mod clipboard;
 mod device;
 mod notification_writer;
 mod sanitizer;
@@ -9,7 +9,7 @@ use notification_writer::NotificationWriter;
 use sanitizer::SwissGermanLanguageMapper;
 use validation::Validator;
 
-//use crate::clipboard::ClipboardTextReader;
+use crate::clipboard::ClipboardTextReader;
 use crate::device::AdminKey;
 
 fn main() -> Result<(), String> {
@@ -19,18 +19,20 @@ fn main() -> Result<(), String> {
 }
 
 struct Application {
-    validator: Validator, //clipboard: ClipboardTextReader
+    validator: Validator,
+    clipboard: ClipboardTextReader,
     notification_writer: NotificationWriter,
 }
 
 impl Application {
     pub fn build() -> Result<Self, String> {
-        //let mut clipboard = ClipboardTextReader::new();
+        let mut clipboard = ClipboardTextReader::new();
         let validator = Validator::new(SwissGermanLanguageMapper::new_boxed());
         let notification_writer = NotificationWriter::new("Admin Key Copy Tool".to_owned());
 
         Ok(Self {
             validator,
+            clipboard,
             notification_writer,
         })
     }
@@ -43,7 +45,7 @@ impl Application {
     fn handle_request(&self) -> Result<String, String> {
         let admin_key = AdminKey::connect()?;
 
-        let data = "".to_string(); // clipboard.read()?;
+        let data = self.clipboard.read()?;
 
         let data = self.validator.validate(data)?;
         let data = self.validator.sanitize(data);
